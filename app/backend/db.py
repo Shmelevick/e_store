2c9cefbef5d7c2d3a65918c2d7291de9f9513337
Mac <yaroslav1992r@gmail.com>
2025-06-27 23:08:29 +0300

    Add db

---
+++ app/backend/db.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+
+engine = create_engine('sqlite:///ecomerce.db', echo=True)
+SessionLocal = sessionmaker(bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+
+
+
