26b134eff27b2d2e98bcfbef86e67ad4b5c5c601
Mac <yaroslav1992r@gmail.com>
2025-07-15 15:47:14 +0300

    Add logger middleware

--- app/main.py
+++ app/main.py
@@ -1,8 +1,28 @@
-from fastapi import FastAPI
+from uuid import uuid4
+from loguru import logger
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 
 from app.routers import auth, category, permission, products, reviews
 
 app = FastAPI()
+
+logger.add("info.log", format="Log: {level} - {message} - {extra[log_id]}:{time}", level="INFO", enqueue=True)
+
+@app.middleware("http")
+async def log_middleware(request: Request, call_next):
+    log_id = str(uuid4())
+    with logger.contextualize(log_id=log_id):
+        try:
+            response = await call_next(request)
+            if response.status_code in (401, 402, 403, 404):
+                logger.warning(f"Request to {request.url.path} failed")
+            else:
+                logger.info('Successfully accessed ' + request.url.path)
+        except Exception as ex:
+            logger.error(f"Request to {request.url.path} failed: {ex}")
+            response = JSONResponse(content={"success": False}, status_code=500)
+        return response
 
 
 @app.get('/')
